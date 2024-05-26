6b60451a8997d8414e876b0abee92063709acac5
mfauzi29 <fauzinetral117@gmail.com>
2024-05-27 02:09:23 +0700

    onboarding page

---
+++ lib/src/screens/Onboarding/onboarding_contents.dart
@@ -0,0 +1,29 @@
+class OnboardingContents {
+  final String title;
+  final String image;
+  final String desc;
+
+  OnboardingContents({
+    required this.title,
+    required this.image,
+    required this.desc,
+  });
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "Monitoring Hewan",
+    image: "assets/images/onboarding1.png",
+    desc: "Memantau dan melacak kondisi serta aktivitas hewan secara real-time.",
+  ),
+  OnboardingContents(
+    title: "Perawatan Kesehatan",
+    image: "assets/images/onboarding2.png",
+    desc: "Membantu pemilik hewan untukmerawat dan memantau kesehatan hewan peliharaan mereka.",
+  ),
+  OnboardingContents(
+    title: "Komunitas Hewan",
+    image: "assets/images/onboarding3.png",
+    desc: "Forum diskusi antar pemilik hewan untuk terhubung, berbagi informasi, pengalaman atau tentang topik topik terkait hewan peliharaan.",
+  ),
+];
